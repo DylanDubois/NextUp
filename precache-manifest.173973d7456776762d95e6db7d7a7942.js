8db7f07a705e3bd0df708c2632d1de013f233ea3
Dyldinski <dubois.a.dylan@gmail.com>
2019-02-17 15:57:54 -0600

    Updates

---
+++ precache-manifest.173973d7456776762d95e6db7d7a7942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NextUp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f6d33fab73b5600ed54",
+    "url": "/NextUp/static/js/runtime~main.1f6d33fa.js"
+  },
+  {
+    "revision": "4185655db972e4b31f19",
+    "url": "/NextUp/static/js/main.4185655d.chunk.js"
+  },
+  {
+    "revision": "a06d563c570948b897b4",
+    "url": "/NextUp/static/js/2.a06d563c.chunk.js"
+  },
+  {
+    "revision": "4185655db972e4b31f19",
+    "url": "/NextUp/static/css/main.f65003bd.chunk.css"
+  },
+  {
+    "revision": "a06d563c570948b897b4",
+    "url": "/NextUp/static/css/2.85d984bd.chunk.css"
+  },
+  {
+    "revision": "1554068cf26b47524e8cfdbb041c4ace",
+    "url": "/NextUp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nextup/precache-manifest.7c51166440d35b806ba5b9c45b34381e.js"
+  "/NextUp/precache-manifest.173973d7456776762d95e6db7d7a7942.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nextup/index.html", {
+workbox.routing.registerNavigationRoute("/NextUp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
