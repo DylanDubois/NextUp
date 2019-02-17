a0c8183327fb6b206ca143787a62d58241f8339b
Dyldinski <dubois.a.dylan@gmail.com>
2019-02-17 15:45:20 -0600

    Updates

---
+++ precache-manifest.7c51166440d35b806ba5b9c45b34381e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nextup/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "788791ca60e1381781c6",
+    "url": "/nextup/static/js/runtime~main.788791ca.js"
+  },
+  {
+    "revision": "9e45f983010023a9eca6",
+    "url": "/nextup/static/js/main.9e45f983.chunk.js"
+  },
+  {
+    "revision": "50dc558726ab356fc5e8",
+    "url": "/nextup/static/js/2.50dc5587.chunk.js"
+  },
+  {
+    "revision": "9e45f983010023a9eca6",
+    "url": "/nextup/static/css/main.f65003bd.chunk.css"
+  },
+  {
+    "revision": "50dc558726ab356fc5e8",
+    "url": "/nextup/static/css/2.85d984bd.chunk.css"
+  },
+  {
+    "revision": "907fa710c5411791f3a2c3999d39ee8c",
+    "url": "/nextup/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "dylandubois.github.io/nextup/precache-manifest.1d70b72c75faed627fa2a6b947d947c8.js"
+  "/nextup/precache-manifest.7c51166440d35b806ba5b9c45b34381e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("dylandubois.github.io/nextup/index.html", {
+workbox.routing.registerNavigationRoute("/nextup/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
