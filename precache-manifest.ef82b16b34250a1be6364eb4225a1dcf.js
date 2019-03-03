7c1deb0513827f09ff1dd28e3ed67dedf5ece796
Dyldinski <dubois.a.dylan@gmail.com>
2019-03-02 19:04:33 -0600

    Updates

---
+++ precache-manifest.ef82b16b34250a1be6364eb4225a1dcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NextUp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f6d33fab73b5600ed54",
+    "url": "/NextUp/static/js/runtime~main.1f6d33fa.js"
+  },
+  {
+    "revision": "37d4ae0ad3c0cfe1b9c0",
+    "url": "/NextUp/static/js/main.37d4ae0a.chunk.js"
+  },
+  {
+    "revision": "e6a13c8a102d247595ab",
+    "url": "/NextUp/static/js/2.e6a13c8a.chunk.js"
+  },
+  {
+    "revision": "37d4ae0ad3c0cfe1b9c0",
+    "url": "/NextUp/static/css/main.b1c1571f.chunk.css"
+  },
+  {
+    "revision": "e6a13c8a102d247595ab",
+    "url": "/NextUp/static/css/2.85d984bd.chunk.css"
+  },
+  {
+    "revision": "3c1065b717a776d26ce9a337aa99b653",
+    "url": "/NextUp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/NextUp/precache-manifest.0cf54261df2bd07728f265cfae37f25b.js"
+  "/NextUp/precache-manifest.ef82b16b34250a1be6364eb4225a1dcf.js"
 );
 
 workbox.clientsClaim();
